8c4d6414c1ec8f3bf55887e480f07e881fb49088
Edwin Ferreira <edwin.ferreira@bitsamericas.com>
2020-02-11 17:46:17 -0500

    cambios dia dos

--- src/main/java/com/apisew/item/ApiItemApplication.java
+++ src/main/java/com/apisew/item/ApiItemApplication.java
@@ -2,10 +2,12 @@ package com.apisew.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient( name = "api-productos" )
+@EnableCircuitBreaker
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class ApiItemApplication {
